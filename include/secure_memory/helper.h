6667b3ea6b4dfbfa78924fc33511e42f55b13217
Alexandros Roussos <thealexroussos@gmail.com>
2017-08-22 05:33:26 +0200

    Removed dead code

--- include/secure_memory/helper.h
+++ include/secure_memory/helper.h
@@ -21,51 +21,17 @@ inline size_t strlen_s(const char *str) {
     return strlen(str);
 }
 
-template<char... digits>
-struct conv2bin;
-
-template<char high, char... digits>
-struct conv2bin<high, digits...> {
-    static_assert(high == '0' || high == '1', "no bin num!");
-    static int const value = (high - '0') * (1 << sizeof...(digits)) +
-                             conv2bin<digits...>::value;
-};
-
-template<char... digits>
-constexpr uint64_t operator "" _b() {
-    return conv2bin<digits...>::value;
-}
-
-template<char high>
-struct conv2bin<high> {
-    static_assert(high == '0' || high == '1', "no bin num!");
-    static uint64_t const value = (high - '0');
-};
-
-/**
- * Helper method for std::hash<> specialisations to combine a proper hash value
- * @tparam T Type of value to hash
- * @param seed Current hash value, will be updated
- * @param v Value to hash
- */
-template <class T>
-inline void hash_combine(std::size_t& seed, const T& v)
-{
-    std::hash<T> hasher;
-    seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
-}
-
 namespace libcom {
-/**
- * Shortcut template to elimate need for "using std::swap" before every swap call.
- * @param one
- * @param two
- */
-template<typename T>
-void swap(T &one, T &two) {
-    using std::swap;
-    swap(one, two);
-}
+    /**
+     * Shortcut template to eliminate need for "using std::swap" before every swap call.
+     * @param one
+     * @param two
+     */
+    template<typename T>
+    void swap(T &one, T &two) {
+        using std::swap;
+        swap(one, two);
+    }
 }
 
 
